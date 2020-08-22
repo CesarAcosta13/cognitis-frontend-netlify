55845b76aaa4e62a80a0bc480979023087f384b1
Cesar Acosta <37489615+CesarAcosta13@users.noreply.github.com>
2020-08-22 20:56:00 -0300

    add folder build

---
+++ build/precache-manifest.9030cae902209ce10cea0dc568115584.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb50b7035dc4a6cc2e21b43df5ac21d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17e2400eeff69da311f3",
+    "url": "/static/css/2.0d2b78f7.chunk.css"
+  },
+  {
+    "revision": "8937f0c19a2ee8abe4c4",
+    "url": "/static/css/main.33174367.chunk.css"
+  },
+  {
+    "revision": "17e2400eeff69da311f3",
+    "url": "/static/js/2.6a77d59b.chunk.js"
+  },
+  {
+    "revision": "be295d85585b7928d0f628c46d20cb57",
+    "url": "/static/js/2.6a77d59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8937f0c19a2ee8abe4c4",
+    "url": "/static/js/main.8da601e3.chunk.js"
+  },
+  {
+    "revision": "6707d241e8e2556c1cbb",
+    "url": "/static/js/runtime-main.c9dc32c1.js"
+  },
+  {
+    "revision": "7d64c61646b9b6b2c138e8cf1f9ea738",
+    "url": "/static/media/cognitis-logo_2020.7d64c616.png"
+  },
+  {
+    "revision": "a9e0a6913372b966cc3994bd2381fde8",
+    "url": "/static/media/cognitisLogo-Texto.a9e0a691.png"
+  },
+  {
+    "revision": "28f9cad9246e4adc6b148f589ec5f9be",
+    "url": "/static/media/companylogo.28f9cad9.png"
+  },
+  {
+    "revision": "e57d1676ed25b39aa1dc5ad1fafa8f5f",
+    "url": "/static/media/imageProductivity.e57d1676.jpeg"
+  },
+  {
+    "revision": "f642fc6044e4ab31574255c395d34750",
+    "url": "/static/media/iphone.f642fc60.jpg"
+  },
+  {
+    "revision": "cd8164e4d7d105d95fe1bea1e9b1cabd",
+    "url": "/static/media/iphone2.cd8164e4.jpg"
+  },
+  {
+    "revision": "5eba7e23b85f86eb33c7bced7357c4aa",
+    "url": "/static/media/iphone3.5eba7e23.jpg"
+  }
+]);
